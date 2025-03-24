9d273fb78e5015564f693760ee4fea229c0b1942
delbempedro <pedrodelbem13@gmail.com>
2025-03-24 07:01:39 -0300

    create
    create data, plot.py and final grafic

---
+++ lista2/plot.py
@@ -0,0 +1,84 @@
+"""
+File: plot.py
+
+Description:
+  Plot the data from the Fortran program for each i interation.
+
+Dependencies:
+- Numpy
+- Matplotlib
+- OS
+
+Since:
+  - 03/2025
+
+Authors:
+  - Pedro C. Delbem <pedrodelbem@usp.br>
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def plot_from_file(filename, output_folder="images"):
+    """
+    Reads a file and creates a simple line plot of the data
+    
+    Parameters
+    ----------
+    filename : str
+        The name of the file to read
+    
+    Returns
+    -------
+    None
+    
+    Notes
+    -----
+    The file should have at least two columns, where the first column is the x data and
+    the second column is the y data. The file is read using np.loadtxt.
+    """
+
+    #Create the output folder if it does not exist
+    os.makedirs(output_folder, exist_ok=True)
+    
+    #Read the file
+    data = np.loadtxt(filename)
+    
+    #Extract the data
+    x = data[:, 0]
+    y = data[:, 1]
+    
+    #Create the plot
+    plt.figure(figsize=(8, 6))
+    plt.plot(x, y, marker='o', linestyle='-', color='b', label='Dados')
+    
+    #Set the labels
+    plt.xlabel('Iterations')
+    plt.ylabel('f(x)')
+    plt.title('Convergence of Newton-Raphson')
+    plt.grid()
+    
+    #Save the plot in the specified folder
+    file_name = os.path.join(output_folder, f"grafic-ex-1.png")
+    plt.savefig(file_name)
+
+    #Close the plot
+    plt.close()
+
+def main():
+    """
+    Main function to execute the plotting of data from a specified file.
+
+    This function uses `plot_from_file` to read data from 'output.txt'
+    and generate a corresponding plot.
+
+    Returns
+    -------
+    None
+    """
+
+    plot_from_file('output.txt')
+
+if __name__ == '__main__':
+    main()
