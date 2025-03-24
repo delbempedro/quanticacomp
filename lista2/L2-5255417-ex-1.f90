e93f1648324512b66bd5ac209ca3aab5ac0878f5
delbempedro <144730623+delbempedro@users.noreply.github.com>
2025-03-24 12:12:27 -0300

    update L2 ex1

--- lista2/L2-5255417-ex-1.f90
+++ lista2/L2-5255417-ex-1.f90
@@ -19,32 +19,63 @@ program newton_raphson
     implicit none
 
     !define variables
-    real x_k, x_kplus1, f_x, df_x
+    real x_k, x_kplus1, f_x_k, df_x_k, f_x(5)
     integer iteration
+
+    write(*,*) "Insert x^4 coefficient:"
+    read(*,*) f_x(5)
+    write(*,*) "Insert x^3 coefficient:"
+    read(*,*) f_x(4)
+    write(*,*) "Insert x^2 coefficient:"
+    read(*,*) f_x(3)
+    write(*,*) "Insert x coefficient:"
+    read(*,*) f_x(2)
+    write(*,*) "Insert constant coefficient:"
+    read(*,*) f_x(1)
 
     !initialize variables
     x_k = 10.0
     x_kplus1 = 0.0
-    f_x = 0.0
-    df_x = 0.0
+    f_x_k = f(x_k,f_x)
+    df_x_k = df(x_k,f_x)
     iteration = 1
 
     open(unit=1, file='output.txt', action='write')
 
-    f_x = x_k**3 - 1
-    df_x = 3*x_k**2
-    x_kplus1 = x_k - f_x/df_x
+    f_x_k = f(x_k,f_x)
+    df_x_k = df(x_k,f_x)
+    x_kplus1 = x_k - f_x_k/df_x_k
     iteration = iteration + 1
-    write(1,*) iteration, x_kplus1**3 - 1
+    write(1,*) iteration, f(x_kplus1,f_x)
 
     !Newton-Raphson method
     do while (abs(x_kplus1 - x_k) > 1e-6)
         x_k = x_kplus1
-        f_x = x_k**3 - 1
-        df_x = 3*x_k**2
-        x_kplus1 = x_k - f_x/df_x
+        f_x_k = f(x_k,f_x)
+        df_x_k = df(x_k,f_x)
+        x_kplus1 = x_k - f_x_k/df_x_k
         iteration = iteration + 1
-        write(1,*) iteration, x_kplus1**3 - 1
+        write(1,*) iteration, f(x_kplus1,f_x)
     end do
+
+    close(1)
+
+contains
+
+    function f(x,f_x) result(result)
+        real, intent(in) :: x
+        real, intent(in) :: f_x(5)
+        real result
+        
+        result = f_x(5)*x**4. + f_x(4)*x**3. + f_x(3)*x**2. + f_x(2)*x + f_x(1)
+    end function f
+
+    function df(x,f_x) result(result)
+        real, intent(in) :: x
+        real, intent(in) :: f_x(5)
+        real result
+        
+        result = 4.*f_x(5)*x**3. + 3.*f_x(4)*x**2. + 2.*f_x(3)*x + f_x(2)
+    end function df
 
 end program newton_raphson
