65536cf1db68de7086d4f375d5d7eaf220d92e74
delbempedro <pedrodelbem13@gmail.com>
2025-04-04 13:58:10 -0300

    finish L3 ex1/ex2

--- lista3/L3-5255417-ex-2.f90
+++ lista3/L3-5255417-ex-2.f90
@@ -19,16 +19,16 @@ program Runge_Kutta
     implicit none
 
     !declare parameters
-    integer, parameter :: n = 1000
+    integer, parameter :: n = 100
     real, parameter :: lambda = -4.0*(2.0*atan(1.0))**2.0
-    real, parameter :: h = 0.01
+    real, parameter :: h = 0.1
 
     !declare variables
     integer :: i
     real y(0:n), v(0:n), k1, k2, k3, k4, l1, l2, l3, l4
 
     !initialize y and v
-    y(0) = 0.0
+    y(0) = 1.0
     v(0) = 0.0
     
     !compute Runge-Kutta method
@@ -45,8 +45,8 @@ program Runge_Kutta
         l4 = lambda*(y(i) + h*k3)
 
         !update y and v
-        y(i+1) = y(i) + (l1 + 2.0*l2 + 2.0*l3 + l4)/6.0
-        v(i+1) = v(i) + (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
+        y(i+1) = y(i) + h*(k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
+        v(i+1) = v(i) + h*(l1 + 2.0*l2 + 2.0*l3 + l4)/6.0
 
     end do
 
