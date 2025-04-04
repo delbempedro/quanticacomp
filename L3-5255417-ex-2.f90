55385b749f18cc060f3750f5d8322e4d5babe5ef
delbempedro <pedrodelbem13@gmail.com>
2025-04-04 12:40:39 -0300

    create L3 dir

---
+++ L3-5255417-ex-2.f90
@@ -0,0 +1,15 @@
+!------------------------------------------------------------
+! File: L2-5255417-ex-3.f90
+!
+! Description:
+!   Solve second order differential equation using Runge-Kutta method
+!
+! Dependencies:
+!   - None
+!
+! Since:
+!   - 03/2025
+!
+! Authors:
+!   - Pedro C. Delbem <pedrodelbem@usp.br>
+!------------------------------------------------------------
