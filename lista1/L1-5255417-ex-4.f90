67256c2c36e83a2ef53c68ca5c2cd1ebd89cf7e7
delbempedro <pedrodelbem13@gmail.com>
2025-04-07 08:33:59 -0300

    finish L1 ex4

--- lista1/L1-5255417-ex-4.f90
+++ lista1/L1-5255417-ex-4.f90
@@ -20,11 +20,11 @@ program exponential_taylor_series
 
     !define variables
     integer i
-    real*16 x(5), e_x, index, next_term, precision
+    real*16 x(5), e_x, index, old_term, next_term, under_precision
 
     !initialize variables
     x = [0.1,1.0,10.0,100.0,1000.0]
-    precision = 1.0e-8
+    under_precision = 10**8
 
     do i=1,5
 
@@ -32,12 +32,13 @@ program exponential_taylor_series
         e_x = 1.0
         index = 1.0
         next_term = 1.0
+        old_term = 1.0
 
         !call compute_exponential
-        call compute_exponential(x(i), e_x, index, next_term, precision)
+        call compute_exponential(x(i), e_x, index, old_term, next_term, under_precision)
 
         !print result
-        write(*,*) "    x = ", x(i)
+        write(*,*) "x = ", x(i)
         write(*,*) "    Computed e^(-", x(i), ") = ", e_x
         write(*,*) "    Real e^(-", x(i), ") = ", exp(-x(i))
 
@@ -45,31 +46,45 @@ program exponential_taylor_series
 
 contains
 
-    subroutine compute_exponential(x, e_x, index, next_term, precision)
+    subroutine compute_exponential(x, e_x, index, old_term, next_term, under_precision)
 
         !deactivate implicit typing
         implicit none
 
         !define variables
         real*16, intent(in) :: x
-        real*16, intent(in) :: precision
+        real*16, intent(in) :: under_precision
         real*16, intent(inout) :: index
         real*16, intent(inout) :: next_term
+        real*16, intent(inout) :: old_term
         real*16, intent(out) :: e_x
+
+        !compute next term
+        old_term = next_term
+        next_term = next_term*(-x)/index
+
+        !update e_x
+        e_x = e_x + next_term
+
+        !update index and factorial
+        index = index + 1.0
         
         !update e_x while the absolute value of next_term is greater than precision
-        do while (abs(next_term) > precision)
+        do while (under_precision*abs(next_term-old_term) > abs(e_x))
 
             !compute next term
+            old_term = next_term
             next_term = next_term*(-x)/index
 
             !update e_x
             e_x = e_x + next_term
 
             !update index and factorial
-            index = index + 1
+            index = index + 1.0
 
         end do
+
+        write(*,*) next_term, old_term, e_x
 
     end subroutine compute_exponential
 
