c8d0fb3ff5b5729864abe26f6baf9fe93c7e92d5
delbempedro <pedrodelbem13@gmail.com>
2025-04-08 16:04:49 -0300

    finish L2 ex4

--- lista2/L2-5255417-ex-4.f90
+++ lista2/L2-5255417-ex-4.f90
@@ -19,8 +19,8 @@ program shooting_method
     implicit none
 
     !define variables
-    real*8 deltax, deltak, phi_deltax, dphi_0, k, phi_xminus1, phi_x, phi_xplus1, x
-    integer i, number_of_iterations
+    real*8 deltax, deltak, phi_deltax, dphi_0, k, phi_xminus1, phi_x, phi_xplus1, x, energy_values(3)
+    integer i, j, number_of_iterations
 
     !define constants
     real*8, parameter :: phi_0 = 0.0d0
@@ -46,82 +46,41 @@ program shooting_method
 
     !define deltax
     deltax = 1.0d0/number_of_iterations
-    
-    !initialize phi_x and phi_xplus1
-    phi_x = 1.0d0
-    phi_xplus1 = 2.0d0
-    
-    !update k until phi(1) >= deltak
-    do while (phi_x*phi_xplus1 > 0.0d0)
 
-        !do the first iteration
-        phi_xminus1 = phi_0
-        phi_x = phi_deltax
-        phi_xplus1 = 2.0d0*phi_x - phi_xminus1 - (k**2.0d0)*(deltax**2.0d0)*phi_x
-        x = deltax
+    do i = 1,3
 
-        !update phi
-        call update_phi(phi_xminus1, phi_x, phi_xplus1, k, deltax, number_of_iterations, x)
+        !initialize phi_x and phi_xplus1
+        phi_xminus1 = 0.1d0
+        phi_x = 1.0d0
+        phi_xplus1 = 2.0d0
 
-        !update k
-        k = k + deltak
+        do while (phi_x*phi_xplus1 > 0.0d0) !update k until phi(1) and phi(1+deltax) have different signs
 
-    end do
+            !do the first iteration
+            phi_xminus1 = phi_0
+            phi_x = phi_deltax
+            phi_xplus1 = 2.0*phi_x - phi_xminus1 - (k**2.0)*(deltax**2.0)*phi_x
+            x = deltax
 
-    write(*,*) "First energy level: ", k
+            !update phi
+            call update_phi(phi_xminus1, phi_x, phi_xplus1, k, deltax, number_of_iterations, x)
 
-    !update k
-    k = k + 2.0*deltak
+            !update k
+            k = k + deltak
 
-    !initialize phi_x and phi_xplus1
-    phi_x = 1.0d0
-    phi_xplus1 = 2.0d0
+        end do
 
-    !second level    
-    do while (phi_x*phi_xplus1 > 0.0d0) !update k until phi(1) >= deltak
-
-        !do the first iteration
-        phi_xminus1 = phi_0
-        phi_x = phi_deltax
-        phi_xplus1 = 2.0*phi_x - phi_xminus1 - (k**2.0)*(deltax**2.0)*phi_x
-        x = deltax
-
-        !update phi
-        call update_phi(phi_xminus1, phi_x, phi_xplus1, k, deltax, number_of_iterations, x)
+        energy_values(i) = k**2.0d0
 
         !update k
-        k = k + deltak
+        k = k + 2.0d0*deltak
 
     end do
-
-    write(*,*) "Second energy level: ", k
-
-    !update k
-    k = k + 2.0*deltak
-
-    !initialize phi_x and phi_xplus1
-    phi_x = 1.0d0
-    phi_xplus1 = 2.0d0
-
-    !third level
-    do while (phi_x*phi_xplus1 > 0.0d0) !update k until phi(1) >= deltak
     
-
-        !do the first iteration
-        phi_xminus1 = phi_0
-        phi_x = phi_deltax
-        phi_xplus1 = 2.0*phi_x - phi_xminus1 - (k**2.0)*(deltax**2.0)*phi_x
-        x = deltax
-
-        !update phi
-        call update_phi(phi_xminus1, phi_x, phi_xplus1, k, deltax, number_of_iterations, x)
-
-        !update k
-        k = k + deltak
-
-    end do
-
-    write(*,*) "Third energy level: ", k
+    !print the energy levels
+    write(*,*) "First energy level: ", energy_values(1), sqrt(energy_values(1))
+    write(*,*) "Second energy level: ", energy_values(2), sqrt(energy_values(2))
+    write(*,*) "Third energy level: ", energy_values(3), sqrt(energy_values(3))
 
 contains
 
@@ -134,7 +93,7 @@ contains
         real*8, intent(inout) :: phi_xminus1, phi_x, phi_xplus1, k, deltax, x
         integer, intent(in) :: number_of_iterations
 
-        do i = 2, number_of_iterations
+        do j = 2, number_of_iterations
             phi_xminus1 = phi_x
             phi_x = phi_xplus1
             phi_xplus1 = 2.0d0*phi_x - phi_xminus1 - (k**2.0d0)*(deltax**2.0d0)*phi_x
