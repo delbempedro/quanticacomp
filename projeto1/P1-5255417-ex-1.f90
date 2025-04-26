7a5b8fe8addff96768d88cc28794a8a130ddff87
delbempedro <pedrodelbem13@gmail.com>
2025-04-26 15:39:48 -0300

    finish P1 ex1

---
+++ projeto1/P1-5255417-ex-1.f90
@@ -0,0 +1,133 @@
+!------------------------------------------------------------
+! File: P1-5255417-ex-1.f90
+!
+! Description:
+!   Solve Poisson's equation using the Numerov algorithm
+!
+! Dependencies:
+!   - None
+!
+! Since:
+!   - 03/2025
+!
+! Authors:
+!   - Pedro C. Delbem <pedrodelbem@usp.br>
+!------------------------------------------------------------
+program Numerov
+
+    !deactivate implicit typing
+    implicit none
+
+    !declare variables
+    integer*8 :: i
+    real*8 :: deltar, phi_i_minus_1, phi_i, phi_i_plus_1, r_i_minus_1, r_i, r_i_plus_1
+
+    !initialize i
+    i = 0
+
+    !define delta r
+    write(*,*) "Insert delta r:"
+    read(*,*) deltar
+
+    !initialize phi(0) and phi(delta r)
+    phi_i_minus_1 = -1.0d0
+    phi_i = -1.0d0 + deltar/2.0d0
+
+    !open files for writing results
+    open(1, file='results-analytic.txt', status='replace')
+    open(2, file='results-zero-infinity.txt', status='replace')
+    open(3, file='results-infinity-zero.txt', status='replace')
+    
+    !compute Numerov method
+    do while (abs(phi_i) > deltar)
+        
+        !compute i
+        i = i + 1
+
+        !compute r_i-1, r_i, r_i+1
+        r_i_minus_1 = (i-1)*deltar
+        r_i         = i*deltar
+        r_i_plus_1  = (i+1)*deltar
+
+        !compute phi(i+1)
+        phi_i_plus_1 = 2.0d0*phi_i - phi_i_minus_1 + (deltar**2.0d0)/12.0d0*( f(r_i_minus_1) + 10.0d0*f(r_i) + f(r_i_plus_1) )
+        phi_i_minus_1 = phi_i
+        phi_i = phi_i_plus_1
+
+        !print analytic solution
+        write(1,'(F12.8,1X,F12.8)') r_i, analytic_solution(r_i)
+
+        !print zero-infinity solution
+        write(2,'(F12.8,1X,F12.8)') r_i, phi_i
+
+    end do
+
+    !compute r_i
+    r_i = (i+1)*deltar
+
+    !print analytic last step
+    write(1,'(F12.8,1X,F12.8)') r_i, analytic_solution(r_i)
+
+    !print last step
+    write(2,'(F12.8,1X,F12.8)') r_i, phi_i
+
+    !close file
+    close(1)
+    close(2)
+
+    !initialize phi(infinity) and phi(infinity-delta r)
+    phi_i_plus_1 = 0.0d0
+    phi_i = 0.0d0
+
+    do while (i>0)
+        
+        !compute i
+        i = i - 1
+
+        !compute r_i-1, r_i, r_i+1
+        r_i_minus_1 = (i-1)*deltar
+        r_i         = i*deltar
+        r_i_plus_1  = (i+1)*deltar
+
+        !compute phi(i+1)
+        phi_i_minus_1 = 2.0d0*phi_i - phi_i_plus_1 + (deltar**2.0d0)/12.0d0*( f(r_i_minus_1) + 10.0d0*f(r_i) + f(r_i_plus_1) )
+        phi_i_plus_1 = phi_i
+        phi_i = phi_i_minus_1
+
+        !print infinity-zero solution
+        write(3,'(F12.8,1X,F12.8)') r_i, phi_i
+
+    end do
+
+    !compute r_i
+    r_i = i*deltar
+
+    !print last step
+    write(3,'(F12.8,1X,F12.8)') r_i, phi_i
+
+    !close file
+    close(3)
+
+contains
+
+    real function f(r)
+
+        !declare parameters
+        real*8, intent(in) :: r
+
+        !compute function
+        f = -r*exp(-r)/2.0d0
+
+    end function f
+
+    real function analytic_solution(r)
+
+        !declare parameters
+        real*8, intent(in) :: r
+
+        !compute function
+        analytic_solution = -exp(-r)*(r/2.0d0 + 1.0d0)
+
+    end function analytic_solution
+
+end program Numerov
---
+++ projeto1/plot.py
@@ -0,0 +1,65 @@
+"""
+plot.py
+
+Module to plot the convergence results of Numerical methods.
+
+Dependencies:
+- Use numpy to read data
+- Use matplotlib to plot
+
+Since:
+- 04/2025
+
+Authors:
+- Pedro C. Delbem. <pedrodelbem@usp.br>
+"""
+import numpy as np # type: ignore
+import matplotlib.pyplot as plt # type: ignore
+
+def read_data(file_name):
+    data = np.loadtxt(file_name)
+    return data[:, 0], data[:, 1]
+
+def main():
+
+    list_of_coord = []
+    list_of_label = ["analytic","zero-infinity","infinity-zero"]
+
+    for i in range(len(list_of_label)):
+        file_name = "results-"+list_of_label[i]+".txt"
+        list_of_coord.append(read_data(file_name))
+
+    print("Insert delta r:")
+    delta_r = float(input())
+
+    plt.figure(figsize=(8, 5))
+
+    for i in range(len(list_of_label)):
+        x, y = list_of_coord[i]
+        label = list_of_label[i]
+        plt.plot(x, y, label=label, marker='o', markersize=3, linestyle='-')
+    
+    plt.xlabel('$r$')
+    plt.ylabel('$\hat{\phi}(r)$')
+    plt.title('All Solutions')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(f'all-plots-{delta_r}.png', dpi=300)
+
+    for i in range(len(list_of_label)):
+        x, y = list_of_coord[i]
+        label = list_of_label[i]
+
+        plt.figure(figsize=(8, 5))
+        plt.plot(x, y, label=label, marker='o', markersize=4, linestyle='-')
+        plt.xlabel('$r$')
+        plt.ylabel('$\hat{\phi}(r)$')
+        plt.title(f'{label}')
+        plt.grid(True)
+        plt.tight_layout()
+        plt.savefig(f'plot-{label}-{delta_r}.png', dpi=300)
+        plt.close()
+
+if __name__ == "__main__":
+    main()
